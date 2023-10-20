73895113e7bb4d40f121f60ee1799fd068d18b33
alimustafa <alimustafa2522@gmail.com>
2023-10-21 04:09:30 +0500

    1st commit in dollar conversion

---
+++ dollar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    float us_dollar;
+    float fee;
+    float BDY;
+    float SDY;
+    float rem_amm;
+
+    
+
+    printf("Enter dollars you want to convert:");
+    scanf("%d",&us_dollar);
+
+
+
+
+
+
+} 
