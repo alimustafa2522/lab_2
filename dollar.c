79b623be32a05092f57e5c2b8a9d07702da1410f
alimustafa <alimustafa2522@gmail.com>
2023-10-21 04:17:03 +0500

    dollar_c modified

--- dollar.c
+++ dollar.c
@@ -4,13 +4,24 @@ int main()
     float us_dollar;
     float fee;
     float BDY;
-    float SDY;
+    float SPY;
     float rem_amm;
 
-    
+
 
     printf("Enter dollars you want to convert:");
-    scanf("%d",&us_dollar);
+    scanf("%f",&us_dollar);
+
+    fee = us_dollar * 10/100;
+    rem_amm = us_dollar - fee;
+
+    BDY = rem_amm/2*0.39;
+    SPY = rem_amm/2*127.65;
+
+    printf("The british pounds will be %f",BDY);
+    printF("The japanese yens is %f",SPY);
+
+
 
 
 
